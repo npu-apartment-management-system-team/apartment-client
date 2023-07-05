3afb714e7af4adde142031b8caa5298f8cde106f
Linxhcy <2452942350@qq.com>
2023-07-05 16:50:59 +0800

    消息详情页面制作

--- src/api/message/index.js
+++ src/api/message/index.js
@@ -11,7 +11,7 @@ export const handleGetMessageList = async (queryDto)=>{
     beginLoading('正在加载消息列表')
     try {
         const {data} = await axios.get(
-            '/api/message/sender/outbox',
+            '/api/message/receiver/inbox',
             {
                 params: queryDto
             }
@@ -21,6 +21,30 @@ export const handleGetMessageList = async (queryDto)=>{
         showNotify({
             type: 'danger',
             message: '加载消息列表失败'
+        })
+    } finally {
+        closeToast()
+    }
+    return null
+}
+
+export const handleGetMessageDetail = async (id)=>{
+    console.log(id)
+    beginLoading('正在加载消息详情')
+    try {
+        const {data} = await axios.get(
+            '/api/message/receiver/inbox/detail',
+            {
+                params: {
+                    id: id
+                }
+            }
+        )
+        return data
+    } catch (e) {
+        showNotify({
+            type: 'danger',
+            message: '加载消息详情失败'
         })
     } finally {
         closeToast()
--- src/views/message/MessageDetail.vue
+++ src/views/message/MessageDetail.vue
@@ -1,11 +1,76 @@
 <script setup>
-
+import {onMounted, ref} from 'vue'
+import {handleGetMessageDetail} from '@/api/message'
+import {showNotify} from 'vant'
+import 'vant/es/notify/style'
+import {useRouter} from "vue-router";
+const router = useRouter()
+const navTitle = ref('')
+const messageDetail = ref({
+  code: 1,
+  msg: '',
+  result: []
+})
+onMounted(async () => {
+  const messageId = router.currentRoute.value.query.id
+  const data = await handleGetMessageDetail(messageId)
+  console.log(data)
+  if (data !== null && data.code === 2000) {
+    messageDetail.value.result = data.result.messageDetail
+    navTitle.value = data.result.senderAdmin.name
+  } else {
+    showNotify({
+      type: 'danger',
+      message: '获取信息详情失败'
+    })
+  }
+})
 </script>
 
 <template>
+  <van-nav-bar
+      :title="navTitle"
+      left-text="返回"
+      left-arrow
+      @click-left="router.go(-1)"
+  >
+  </van-nav-bar>
+  <div class="message-container">
+    <van-list>
+      <div id="time">
+        {{messageDetail.result.createTime}}
+      </div>
+      <van-cell-group inset style="margin-top:2%">
+        <van-col>
+          {{messageDetail.result.message}}
+        </van-col>
+      </van-cell-group>
+    </van-list>
+  </div>
 
 </template>
 
 <style scoped lang="less">
-
+#delete{
+  color: red;
+}
+.message-container{
+  margin-top: 7%;
+  #time{
+    text-align: center;
+    margin: 0 auto;
+    backface-visibility: hidden;
+    color:darkgray;
+  }
+  .van-cell-group{
+    width: 95%;
+    margin: 0 auto;
+    .van-col{
+      text-align: left;
+      margin-left: 5%;
+      color: gray;
+      word-break: break-all;
+    }
+  }
+}
 </style>
