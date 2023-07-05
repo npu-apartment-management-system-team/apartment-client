91be292a60217bdd48922167e651aa0ad803227a
WangMinan <WangMinan@users.noreply.github.com>
2023-07-05 03:55:27 +0000

    deploy: 8fe95763f0126c9a6e7da7f781d583612cc1dcdf

--- assets/FinanceDetail-1c1dd681.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./vue-router-05758286.js";import{b as a,c as n}from"./@vue-802b571a.js";const p={__name:"FinanceDetail",setup(u){const e=r(),t=a({id:0,price:0,status:"",type:"",createTime:"",updateTime:""});return n(()=>{t.value=JSON.parse(e.currentRoute.value.query.finance)}),(c,o)=>null}};export{p as default};
